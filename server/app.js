c7dec156c3c04e24d5b653eef66e9956d859a2fb
ankit07-byte <ankman1012018@gmail.com>
2025-08-04 20:18:13 +0530

    modified:   client/.env 	modified:   server/app.js

--- server/app.js
+++ server/app.js
@@ -22,10 +22,22 @@ const app = express();
 
 // Middleware
 app.use(helmet());
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://complaint-management-plum.vercel.app/' // your Vercel frontend URL
+];
+
 app.use(cors({
-  origin: process.env.CLIENT_URL,
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true
 }));
+
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cookieParser());
